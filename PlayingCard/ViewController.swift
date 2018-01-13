05406058ea20de61b46797d17088f5a6dfd9230c
devxris <chuwei0915@gmail.com>
2018-01-13 22:55:41 +0800

    Initial Commit

---
+++ PlayingCard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by Chris Huang on 13/01/2018.
+//  Copyright © 2018 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
