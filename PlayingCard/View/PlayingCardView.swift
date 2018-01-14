88e431ef93f4f040fcfa97338495846ba1c12767
devxris <chuwei0915@gmail.com>
2018-01-14 15:49:17 +0800

    Lecture 6: Drawing - roundedRect ( UIBezierPath ), cornerLabels ( UILabel as subview ), and autolayout

--- PlayingCard/View/PlayingCardView.swift
+++ PlayingCard/View/PlayingCardView.swift
@@ -10,47 +10,117 @@ import UIKit
 
 class PlayingCardView: UIView {
 	
-	override init(frame: CGRect) {
-		super.init(frame: frame)
-		setContentMode()
+	// MARK: Properties
+	
+	// setNeedsDisplay() is for calling draw(_ rect); setNeedsLayout() is for sub view calling layoutSubviews()
+	var rank: Int = 5 { didSet { setNeedsDisplay(); setNeedsLayout() } }
+	var suit: String = "♥️" { didSet { setNeedsDisplay(); setNeedsLayout() } }
+	var isFaceUp: Bool = true { didSet { setNeedsDisplay(); setNeedsLayout() } }
+	
+	// MARK: Private properties and funcs
+	
+	// draw corner symbols by UILabel() (subView) with NSAttributedString
+	private lazy var upperLeftCornerLabel: UILabel = createCornerLabel()
+	private lazy var lowerRightCornerLabel: UILabel = createCornerLabel()
+	
+	private func createCornerLabel() -> UILabel {
+		let label = UILabel()
+		label.numberOfLines = 0
+		addSubview(label)
+		return label
 	}
 	
-	required init?(coder aDecoder: NSCoder) {
-		super.init(coder: aDecoder)
-		setContentMode()
+	private func configureCornerLabel(_ label: UILabel ) {
+		label.attributedText = cornerString
+		label.frame.size = .zero // clear out its size
+		label.sizeToFit()        // size label to fit its content
+		label.isHidden = !isFaceUp
 	}
 	
-	// or set in Interface Builder for bounds change, eg. rotation
-	private func setContentMode() { contentMode = .redraw }
-
-	// Draw a circle with UIGraphicsGetCurrentContext() & UIBezierPath()
+	// corner labels helper properties
+	private var cornerString: NSAttributedString {
+		return centeredAttributedString(rankString+"\n"+suit, fontSize: cornerFontSize)
+	}
+	
+	// corner labels helper funcs
+	private func centeredAttributedString(_ string: String, fontSize: CGFloat) -> NSAttributedString {
+		var font = UIFont.preferredFont(forTextStyle: .body).withSize(fontSize)
+		font = UIFontMetrics(forTextStyle: .body).scaledFont(for: font) // adapt dynamic type and accessibility
+		let paragraphStyle = NSMutableParagraphStyle()
+		paragraphStyle.alignment = .center
+		return NSAttributedString(string: string, attributes: [.font: font, .paragraphStyle: paragraphStyle])
+	}
+	
+	// MARK: Position Labels
+	override func layoutSubviews() {
+		super.layoutSubviews()
+		
+		configureCornerLabel(upperLeftCornerLabel)
+		upperLeftCornerLabel.frame.origin = bounds.origin.offsetBy(dx: cornerOffset, dy: cornerOffset)
+		
+		configureCornerLabel(lowerRightCornerLabel)
+		lowerRightCornerLabel.transform = CGAffineTransform.identity
+			.rotated(by: CGFloat.pi) // rotation is anchored at upperleft corner, origin
+			.translatedBy(x: lowerRightCornerLabel.frame.size.width,
+						  y: lowerRightCornerLabel.frame.size.height)
+		lowerRightCornerLabel.frame.origin = CGPoint(x: bounds.maxX, y: bounds.maxY)
+			.offsetBy(dx: -cornerOffset, dy: -cornerOffset)
+			.offsetBy(dx: -lowerRightCornerLabel.frame.size.width, dy: -lowerRightCornerLabel.frame.size.height)
+	}
+	
+	// for dyanmic type and accessibility to reflect setting changes immediately
+	override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+		super.traitCollectionDidChange(previousTraitCollection)
+		setNeedsDisplay()
+		setNeedsLayout()
+	}
+	
+	// MARK: Draw
+	
 	override func draw(_ rect: CGRect) {
-		
-		// circle with UIGraphicsGetCurrentContext()
-		if let context = UIGraphicsGetCurrentContext() {
-			context.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY),
-						   radius: 100,
-						   startAngle: 0,
-						   endAngle: CGFloat.pi * 2,
-						   clockwise: true)
-			context.setLineWidth(5.0)
-			UIColor.green.setFill()
-			UIColor.red.setStroke()
-			context.strokePath()
-			context.fillPath() // won't run because path is consumed by strokePath()
-		}
-		
-		// circle with UIBezierPath()
-		let path = UIBezierPath()
-		path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY),
-					radius: 100,
-					startAngle: 0,
-					endAngle: CGFloat.pi * 2,
-					clockwise: false)
-		path.lineWidth = 5.0
-		UIColor.green.setFill()
-		UIColor.red.setStroke()
-		path.stroke()
-		path.fill()
+		// draw rounded rect card
+		let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
+		roundedRect.addClip() // draw inside clipping area
+		UIColor.white.setFill() // default is white as well, so set clear color in storyboard
+		roundedRect.fill()
 	}
+}
+
+extension PlayingCardView {
+	private struct SizeRatio {
+		static let cornerFontSizeToBoundsHeight: CGFloat = 0.085
+		static let cornerRadiusToBoundsHeight: CGFloat = 0.06
+		static let cornerOffsetToCornerRadius: CGFloat = 0.33
+		static let faceCardImageSizeToBoundsSize: CGFloat = 0.75
+		static let cardBackImageSizeToBoundsSize: CGFloat = 1.2
+	}
+	private var cornerRadius: CGFloat { return bounds.size.height * SizeRatio.cornerRadiusToBoundsHeight }
+	private var cornerOffset: CGFloat { return cornerRadius * SizeRatio.cornerOffsetToCornerRadius }
+	private var cornerFontSize: CGFloat { return bounds.size.height * SizeRatio.cornerFontSizeToBoundsHeight }
+	private var rankString: String {
+		switch rank {
+		case 1     : return "A"
+		case 2...10: return String(rank)
+		case 11	   : return "J"
+		case 12    : return "Q"
+		case 13    : return "K"
+		default    : return "?"
+		}
+	}
+}
+
+extension CGRect {
+	var leftHalf: CGRect { return CGRect(x: minX, y: minY, width: width/2, height: height) }
+	var rightHalf: CGRect { return CGRect(x: midX, y: minY, width: width/2, height: height) }
+	func inset(by size: CGSize) -> CGRect { return insetBy(dx: size.width, dy: size.height) }
+	func sized(to size: CGSize) -> CGRect { return CGRect(origin: origin, size: size) }
+	func zoom(by scale: CGFloat) -> CGRect {
+		let newWidth = width * scale
+		let newHeight = height * scale
+		return insetBy(dx: (width - newWidth) / 2, dy: (height - newHeight) / 2)
+	}
+}
+
+extension CGPoint {
+	func offsetBy(dx: CGFloat, dy: CGFloat) -> CGPoint { return CGPoint(x: x + dx, y: y + dy) }
 }
